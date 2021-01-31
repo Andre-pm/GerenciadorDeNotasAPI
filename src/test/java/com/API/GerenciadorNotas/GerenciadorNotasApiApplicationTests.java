d620f60887c6bea8622e4cba8b3f801c8b49dc32
And <andre.pereira.martins100@gmail.com>
2021-01-31 06:52:54 -0300

    Correção da pasta

---
+++ src/test/java/com/API/GerenciadorNotas/GerenciadorNotasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.GerenciadorNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorNotasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
